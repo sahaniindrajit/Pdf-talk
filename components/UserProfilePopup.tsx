5ba47f815fc720ae033790c087d54e03bafe3815
Indrajit sahani <indrajit38mig@gmail.com>
2024-10-08 22:01:23 +0530

    added navbar and userprofile popup card

---
+++ components/UserProfilePopup.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useRef, useEffect } from 'react';
+import Image from 'next/image';
+import { useSession, signOut } from "next-auth/react";
+
+const UserProfilePopup = () => {
+  const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
+  const popupRef = useRef<HTMLDivElement | null>(null);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+
+    if (!popupRef?.current?.contains(event.target as Node)) {
+      handleClose();
+    }
+
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+
+    };
+  });
+
+  return (
+    <div
+      className="relative inline-block">
+      <button
+        onClick={handleToggle}
+        className="bg-transparent hover:bg-transparent rounded"
+      >
+        {session?.user?.image && (
+          <Image
+            src={session.user.image}
+            alt="User Image"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+        )}
+      </button>
+
+      {isOpen && (
+        <div
+          ref={popupRef}
+          className="absolute z-10 w-72 p-4  bg-white rounded-lg shadow-lg mt-2 overflow-hidden"
+          style={{
+            left: '-270%',
+            transform: 'translateX(-50%)',
+            top: 'calc(100% + 8px)',
+            maxHeight: '80vh',
+            overflowY: 'auto',
+          }}
+        >
+          <div className="flex items-center mb-4">
+            {session?.user?.image && (
+              <Image
+                src={session.user.image}
+                alt="User Avatar"
+                width={40}
+                height={40}
+                className="rounded-full mr-2"
+              />
+            )}
+            <div className="flex flex-col overflow-hidden">
+              <h2 className="font-bold text-black overflow-ellipsis overflow-hidden whitespace-nowrap">{session?.user?.name}</h2>
+              <p className="text-gray-600 text-sm overflow-ellipsis overflow-hidden whitespace-nowrap">{session?.user?.email}</p>
+            </div>
+          </div>
+          <button
+            onClick={() => signOut()}
+            className="w-full bg-red-500 text-white py-2 rounded hover:bg-red-600 transition"
+          >
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserProfilePopup;
---
+++ components/navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FileText } from "lucide-react"
+import UserProfilePopup from './UserProfilePopup';
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
+                <div className="flex items-center">
+                    <div className="w-10 h-10 bg-red-500 mr-2 pixel-border flex items-center justify-center" style={{ boxShadow: 'inset 2px 2px 0 2px #000' }}>
+                        <FileText className="text-white w-6 h-6" />
+                    </div>
+                    <span className="text-xl font-bold">PDF TALK</span>
+                </div>
+
+                <div className='flex items-center'>
+                    <UserProfilePopup />
+
+                </div>
+
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
