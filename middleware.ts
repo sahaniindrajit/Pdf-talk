7059be507100a97761f473317593fbb0cbd8ab1a
Indrajit sahani <indrajit38mig@gmail.com>
2024-10-09 10:07:03 +0530

    protected /dashboard route

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse, NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export async function middleware(req: NextRequest) {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+    const { pathname } = req.nextUrl;
+
+    if (!token && pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/signin', req.url));
+    }
+
+    if (token && (pathname === '/signin')) {
+        return NextResponse.redirect(new URL('/dashboard', req.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/dashboard/:path*', '/signin', '/signup'],
+};
