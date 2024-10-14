0dd1168ce7d64d2cf6fec4c4627cc1580a3d59a5
Indrajit sahani <indrajit38mig@gmail.com>
2024-10-14 18:25:36 +0530

    solving vercel middleware issue

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
             'lh3.googleusercontent.com'       // Google avatars
         ],
     },
+    experimental: {
+        runtime: 'nodejs',
+    },
 };
 
 export default nextConfig;
