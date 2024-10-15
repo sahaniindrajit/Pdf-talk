c42b31333ef77bbc937e50c93f53f5c7ea5f9d96
Indrajit sahani <indrajit38mig@gmail.com>
2024-10-16 00:49:30 +0530

    removed console log

--- actions/textProcessing.ts
+++ actions/textProcessing.ts
@@ -10,25 +10,17 @@ export async function generateChunks(docsURL: string) {
         throw new Error("Download URL not found");
     }
 
-    console.log("----Download URL Found Successfully----");
-
     const response = await fetch(downloadURL);
 
     const data = await response.blob();
-
-    console.log("----Loading PDF Document----");
 
     const pdfLoader = new PDFLoader(data);
 
     const docs = await pdfLoader.load();
 
-    console.log("----Splitting Document into smaller chunks----");
-
     const splitter = new RecursiveCharacterTextSplitter();
 
     const chunks = await splitter.splitDocuments(docs);
-
-    console.log(`----Splitting Done, ${chunks.length} chunks found----`);
 
     return chunks;
 }
